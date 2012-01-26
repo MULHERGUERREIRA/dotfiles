e9facb27138b7009e5180dc9351629920abc1f69
Chris Kelly <amateurhuman@gmail.com>
2012-01-25 20:22:07 -0800

    Aliases, backup scripts, other items, oh my

---
+++ elastic-search/aliases.zsh
@@ -0,0 +1,1 @@
+alias elasticsearch-start='elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.18.6/config/elasticsearch.yml'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias whatsmyip='curl -s ifconfig.me | tr -d '\n' | pbcopy | echo `pbpaste`'
 alias ..='cd ..'
+alias serve='python -m SimpleHTTPServer'
