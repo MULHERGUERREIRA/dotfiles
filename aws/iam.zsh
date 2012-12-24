89f1b9c7995e338b1a5ab38bff08156aed4dfea6
Chris Kelly <amateurhuman@gmail.com>
2012-12-23 17:54:02 -0800

    Add some elastic beanstalk love

--- aws/iam.zsh
+++ aws/iam.zsh
@@ -1,7 +1,6 @@
 # AWS Identity and Access Management Configuration
 # Download CLI at http://aws.amazon.com/developertools/AWS-Identity-and-Access-Management/4143
 
-PATH="$HOME/.aws/iam/bin:$PATH"
-
-export AWS_IAM_HOME="$HOME/.aws/iam"
-export AWS_CREDENTIAL_FILE="$ZSH/secret/aws/iam/ckelly_cred.txt"
+export AWS_IAM_HOME="/usr/local/lib/iam-cli"
+export PATH="$AWS_IAM_HOME/bin:$PATH"
+export AWS_CREDENTIAL_FILE="$HOME/.elasticbeanstalk/aws_credential_file"
---
+++ aws/path.zsh
@@ -0,0 +1,1 @@
+PATH="/usr/local/lib/elasticbeanstalk-cli/eb/macosx/python2.7:$PATH"
